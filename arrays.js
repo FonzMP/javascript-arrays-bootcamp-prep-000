c2a579f5704c4392049eb64abe71d5e3fa37da28
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-16 05:38:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  removed = array.shift();
-  return array;
+  
 }
 
 function destructivelyRemoveElementFromEndOfArray() {
