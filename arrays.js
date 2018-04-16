cd4cd0069a9df83e2ed4751d9a46eff5b596f0ab
Alexander Keith Wilson <fonzmp@gmail.com>
2018-04-16 05:09:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, e) {
+  let newArr = arr.unshift(e);
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, e) {
+  arr.unshift(e);
+  return arr;
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray() {
+
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {
+
+}
+
+function removeElementFromBeginningOfArray() {
+
+}
+
+function destructivelyRemoveElementFromEndOfArray() {
+
+}
+
+function removeElementFromEndOfArray() {
+
+}
